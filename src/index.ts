867a18be4461cc7272f0aa62de63e554349aba92
Jayaraj <jayaraj.sivadasan@gmail.com>
2021-10-30 13:47:03 -0400

    separated collection and sorting logic

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-class Sorter {
-    constructor(public collection: number[]){  }
-    sort(): void {
-        const {length} = this.collection;
-        for(let i=0; i<length; i++){ // full iteration
+import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from "./Sorter";
 
-                for (let j=0; j< length - i -1;j ++ ){
-                    if(this.collection[j] > this.collection[j+1]){
-                        const lhsElement = this.collection[j];
-                        this.collection[j]=this.collection[j+1];
-                        this.collection[j+1]=lhsElement; 
-                    }
-                }
-        }
-    }
-}
-
-const sorter = new Sorter([10,3,-5,0, 12, 289,33,13,65, 78, 43, 21,91]);
+const numbersCollection =new NumbersCollection([10,3,-5,0, 12, 289,33,13,65, 78, 43, 21,91]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
